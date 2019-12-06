43bc87e2f1809d7c08205261b3a42083233ca74f
Felicia Wilkes <feliciankc@gmail.com>
2019-12-06 15:39:55 -0600

    Creating the Fetch Posts with Query Action

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,8 +3,10 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import reducers from "./reducers";
+
 import thunk from "redux-thunk";
+
+import reducers from "./reducers";
 
 const createStoreWithMiddleware = applyMiddleware(thunk)(
   compose(
