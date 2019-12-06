6c986b82d57891fcd56909ae485477d92ef4cd4b
Felicia Wilkes <feliciankc@gmail.com>
2019-12-05 21:56:31 -0600

    Using the Redux Map State to Props Function in the Recent Posts Component

--- src/components/recentPosts.js
+++ src/components/recentPosts.js
@@ -1,27 +1,38 @@
 import React, { Component } from "react";
 
 import { connect } from "react-redux";
+
 import * as actions from "../actions";
 
 class RecentPosts extends Component {
   componentDidMount() {
     this.props.fetchRecentPosts();
   }
+  renderPosts = function() {
+    const posts = this.props.recentPosts.map((post, index) => {
+      if (index < 3) {
+        return <li key={index}>{post.title}</li>;
+      }
+    });
+    return posts;
+  };
 
   render() {
     return (
       <div className="recent-posts">
         <div className="recent-posts_wrapper">
           <div className="recent-posts_heading">Recent Posts</div>
-          <ul className="recent-posts_posts">
-            <li>recent post 0</li>
-            <li>recent post 1</li>
-            <li>recent post 2</li>
-          </ul>
+          <ul className="recent-posts_posts"></ul>
+          {this.renderPosts()}
         </div>
       </div>
     );
   }
 }
 
-export default connect(null, actions)(RecentPosts);
+function mapStateToProps(state) {
+  return {
+    recentPosts: state.posts.recentPosts
+  };
+}
+export default connect(mapStateToProps, actions)(RecentPosts);
