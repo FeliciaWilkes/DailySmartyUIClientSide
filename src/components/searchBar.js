54a3cbba357ed2544bbceacfbc64ca5b6357def7
Felicia Wilkes <feliciankc@gmail.com>
2019-12-07 00:21:52 -0600

    .search-bar__home {

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -21,14 +21,13 @@ class SearchBar extends Component {
 
     return (
       <form
-        className="search-bar"
+        className={`search-bar search-bar__${this.props.page}`}
         onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}
       >
         <div className="search-bar__wrapper">
           <Field name="query" component={this.renderInput} />
           <p>Press return to search</p>
         </div>
-        <Field name="query" component={this.renderInput} />
       </form>
     );
   }
